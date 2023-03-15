b833f017767e65c972eab631c4586fb44a7a3822
Elias <eliasmenapech@gmail.com>
2023-03-15 12:00:00 -0600

    Se agrego firebase a la aplicacion para authentication

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidHilt) apply false
-   // alias(libs.plugins.googleService) apply false
+    alias(libs.plugins.googleService) apply false
 }
