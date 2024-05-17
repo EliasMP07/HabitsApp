738e2c63870bb72d27c4fc70e97446ee81a7a90a
Elias <eliasmenapech@gmail.com>
2024-05-16 22:44:13 -0600

    Se agrego inicio por google

--- app/src/main/java/com/devdroid/habitsapp/authentication/data/repository/AuthenticationFirebaseSource.kt
+++ app/src/main/java/com/devdroid/habitsapp/authentication/data/repository/AuthenticationFirebaseSource.kt
@@ -1,12 +1,28 @@
 package com.devdroid.habitsapp.authentication.data.repository
 
+import android.content.Context
+import com.devdroid.habitsapp.R
 import com.devdroid.habitsapp.authentication.domain.repository.AuthenticationDataSource
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.tasks.await
 import java.lang.Exception
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
-class AuthenticationFirebaseSource : AuthenticationDataSource{
+class AuthenticationFirebaseSource(
+    private val firebaseAuth: FirebaseAuth,
+    @ApplicationContext private val context: Context
+): AuthenticationDataSource{
     override suspend fun login(email: String, password: String): Result<Unit> {
         return try {
             Firebase.auth.signInWithEmailAndPassword(email, password).await()
@@ -22,6 +38,37 @@ class AuthenticationFirebaseSource : AuthenticationDataSource{
             Result.success(Unit)
         }catch (e: Exception){
             Result.failure(e)
+        }
+    }
+
+    override suspend fun loginWithGoogle(idToken: String): Result<Unit> {
+        return try {
+            val credential = GoogleAuthProvider.getCredential(idToken, null)
+            completeRegisterWithCredential(credential)
+            Result.success(Unit)
+        }catch (e: Exception){
+            e.printStackTrace()
+            Result.failure(e)
+        }
+    }
+
+    //Funcion que recupera el cliente de google
+   /* override fun getGoogleClient(): GoogleSignInClient {
+        val gso = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN
+        ).requestIdToken(context.getString(R.string.default_web_client_id)).requestEmail()
+            .build()
+        return GoogleSignIn.getClient(context, gso)
+    }*/
+
+    //Funcion que completa el registro con la credencial de la cuenta
+    private suspend fun completeRegisterWithCredential(credential: AuthCredential): FirebaseUser? {
+        return suspendCancellableCoroutine { cancellableContinuation ->
+            firebaseAuth.signInWithCredential(credential).addOnSuccessListener {
+                cancellableContinuation.resume(it.user)
+            }.addOnFailureListener {
+                cancellableContinuation.resumeWithException(it)
+            }
         }
     }
 
